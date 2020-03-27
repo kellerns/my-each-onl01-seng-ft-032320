d78c4c988d81dbdfe20bf5637d6573006fdbb4a0
Nicholas Keller <nic.keller328@gmail.com>
2020-03-27 17:58:24 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -3,7 +3,12 @@ def my_each(array) # put argument(s) here
   if block_given?
     i = 0
     
-    
+    while i < array.length
+      yield
+      i += 1
+    end
+  else
+  end
   
   # code here
 end
