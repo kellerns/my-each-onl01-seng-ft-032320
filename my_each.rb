7dcc243ca11da5e6c449f15034e95ad7bb48527b
Nicholas Keller <nic.keller328@gmail.com>
2020-03-27 18:03:01 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,15 +2,16 @@ def my_each(array) # put argument(s) here
 
   if block_given?
     i = 0
+    new_array = []
     
     while i < array.length
       yield
-      array[i]
+      new_array << array[i]
       i += 1
     end
+    new_array
   else
   end
   
-  array
   # code here
 end
