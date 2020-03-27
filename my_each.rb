eed9eec782551785f86e1faf8d2fb2c9b1edf79d
Nicholas Keller <nic.keller328@gmail.com>
2020-03-27 18:08:41 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array) # put argument(s) here
+
+  if block_given?
+    i = 0
+    new_array = []
+    
+    while i < array.length
+      yield array[i]
+      new_array << array[i]
+      i += 1
+    end
+    new_array
+  else
+  end
+ 
 end
